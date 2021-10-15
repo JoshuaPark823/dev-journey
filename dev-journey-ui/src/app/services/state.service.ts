2d77f907c74a23b111c8ee07f6a6d0131ef60432
Joshua Park <joshuapark823@gmail.com>
2021-10-14 21:01:06 -0400

    gradient test, styles

--- dev-journey-ui/src/app/services/state.service.ts
+++ dev-journey-ui/src/app/services/state.service.ts
@@ -1,9 +1,12 @@
+import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+
+  darkModeSource$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor() { }
 
